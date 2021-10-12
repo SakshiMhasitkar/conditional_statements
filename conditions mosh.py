a0704076a0a73fd4d73e50de63dc766c1680c51d
Sakshi <92329377+SakshiMhasitkar@users.noreply.github.com>
2021-10-12 19:51:29 +0530

    Add files via upload

---
+++ conditions mosh.py
@@ -0,0 +1,13 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day.")
+    print("Eat ice.")
+elif is_cold:
+    print("It's a cold day.")
+    print("Drink coffee.")
+else:
+    print("It's the perfect day.")
+
+print("Enjoy your day.")
